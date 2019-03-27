98a7dafa35d05b7dfe106ce0b3938631c51d4c24
Allen Pan <allenpan@Allens-MacBook-Air.local>
2019-03-27 19:32:14 -0400

    add questions

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,15 @@ from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import app, db
-from app.models import User
+from app.models import User, Response
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -61,6 +64,53 @@ def logout():
     return redirect("/")
 
 
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    """Fill out questionnaire"""
+
+    if request.method == "POST":
+        q1 = request.form.get("q1")
+        q2 = request.form.get("q2")
+        q3 = request.form.get("q3")
+        q4 = request.form.get("q4")
+        q5 = request.form.get("q5")
+        q6 = request.form.get("q6")
+        q7 = request.form.get("q7")
+        q8 = request.form.get("q8")
+        q9 = request.form.get("q9")
+        q10 = request.form.get("q10")
+        q11 = request.form.get("q11")
+        q12 = request.form.get("q12")
+        q13 = request.form.get("q13")
+        q14 = request.form.get("q14")
+        q15 = request.form.get("q15")
+        q16 = request.form.get("q16")
+        q17 = request.form.get("q17")
+        q18 = request.form.get("q18")
+        q19 = request.form.get("q19")
+        q20 = request.form.get("q20")
+        q21 = request.form.get("q21")
+        q22 = request.form.get("q22")
+        q23 = request.form.get("q23")
+        q24 = request.form.get("q24")
+        q25 = request.form.get("q25")
+        q26 = request.form.get("q26")
+        q27 = request.form.get("q27")
+        q28 = request.form.get("q28")
+        q29 = request.form.get("q29")
+        q30 = request.form.get("q30")
+        author = User.query.filter_by(id=session["user_id"]).first()
+
+        new_response = Response(q1=q1, q2=q2, q3=q3, q4=q4, q5=q5, q6=q6, q7=q7, q8=q8, q9=q9, q10=q10, \
+        q11=q11, q12=q12, q13=q13, q14=q14, q15=q15, q16=q16, q17=q17, q18=q18, q19=q19, q20=q20, \
+        q21=q21, q22=q22, q23=q23, q24=q24, q25=q25, q26=q26, q27=q27, q28=q28, q29=q29, q30=q30, author=author)
+        db.session.add(new_response)
+        db.session.commit()
+
+        return redirect("/")
+    else:
+        return render_template("predict.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -103,3 +153,9 @@ def register():
     # User reached route via GET
     else:
         return render_template("register.html")
+
+@app.route("/rules", methods=["GET", "POST"])
+def rules():
+    """Rules"""
+
+    return render_template("rules.html")
